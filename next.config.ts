8f74b63c5475494a99e57d922a8e533b6d1747ef
Tech Lead - Light <thanat.light@schoolbright.co>
2026-03-14 15:25:53 +0700

    ✨ ระบบ Configuration : เพิ่มการตั้งค่า experimental สำหรับการปรับปรุงการนำเข้าแพ็คเกจ (หน้าที่แก้ไข /next.config.ts)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: ["antd", "@ant-design/icons", "lucide-react"],
+  },
   images: {
     remotePatterns: [
       {
